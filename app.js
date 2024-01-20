726418724ef3ac549f39f46dd787f0e7b07c4686
Apeksha Bhausaheb LOkhande <apekshalokhande8600@gmail.com>
2024-01-20 19:26:22 +0530

    schema validation is updated

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ const MethodOveride=require("method-override");
 const ejsMate=require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError=require("./utils/ExpressError.js");
-const {listingSchema}= require ("./schema.js")
+
+const {listingSchema}=require("./schema.js");
 
 main().
 then((res)=>{
@@ -41,6 +42,17 @@ app.get("/",(req,res)=>{
     res.send("your port is working ");
 });
 
+const validateListing=(req,res,next)=>{
+    let {error}=listingSchema.validate(req.body);
+    
+    if(error){
+        let errMsg=error.details.map((el)=>el.message).join(",");
+        throw new ExpressError(400,errMsg);
+    }else{
+        next();
+    }
+};
+
 //index route
 app.get("/listings",wrapAsync(async(req,res)=>{
     const allListings=await Listing.find({});
@@ -60,12 +72,8 @@ app.get("/listings/:id",wrapAsync(async(req,res)=>{
 }));
 
 //create route
-app.post("/listings",wrapAsync(async(req,res,next)=>{
-    const result=listingSchema.validate(req.body);
-    console.log(result);
-    if(result.error){
-        throw new ExpressError(400,result.error);
-    }
+app.post("/listings",validateListing,wrapAsync(async(req,res,next)=>{
+    
     
      const newListing=new Listing(req.body.listing);
      
@@ -86,10 +94,8 @@ app.get("/listings/:id/edit",wrapAsync(async(req,res)=>{
 }));
 
 //update route
-app.put("/listings/:id",wrapAsync(async (req,res)=>{
-    if(!req.body.listing){
-        throw new ExpressError(480,"send valid data for listings")
-       }
+app.put("/listings/:id",validateListing,wrapAsync(async (req,res)=>{
+    
     let{id}=req.params;
     await Listing.findByIdAndUpdate(id,{...req.body.listing});
     res.redirect(`/listings/${id}`);
